b3ed86a2d75ff069b9ff31afea10c0b9f8ee1f1f
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-06-02 12:34:41 -0400

    Cambia estrctura del repositorio

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the repository to analyze the FONASA's GRD's data of hospital discharges in Chile. ',
+    author='Javier Rojas',
+    license='MIT',
+)
