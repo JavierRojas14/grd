3b39956c3a637b15119ac1fd8095d307a54a7a5a
Javier Rojas <javierrojas14.jr@gmail.com>
2023-09-01 15:22:44 -0400

    Obtiene total de procedimientos

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,23 +1,26 @@
+import numpy as np
+
+
 def contar_procedimientos_y_rankear(df_procedimientos_long):
     conteo_procedimientos = (
-        df_procedimientos_long.groupby(["ANIO_EGRESO", "COD_HOSPITAL"])["value"]
+        df_procedimientos_long.groupby(["ANIO_EGRESO", "COD_HOSPITAL"])["procedimiento"]
         .value_counts()
         .reset_index(name="conteo")
-        .sort_values(["ANIO_EGRESO", "value", "conteo"], ascending=False)
+        .sort_values(["ANIO_EGRESO", "procedimiento", "conteo"], ascending=False)
     )
 
-    conteo_procedimientos["ranking"] = (
-        conteo_procedimientos.groupby(["ANIO_EGRESO", "value"]).cumcount() + 1
+    ranking_por_procedimiento = (
+        conteo_procedimientos.groupby(["ANIO_EGRESO", "procedimiento"]).cumcount() + 1
     )
 
-    conteo_procedimientos["diferencia_proximo_hospital"] = conteo_procedimientos["conteo"].diff(
-        periods=-1
-    )
+    diferencia_entre_hospitales_contiguos = conteo_procedimientos["conteo"].diff(periods=-1)
 
-    conteo_procedimientos["diferencia_proximo_hospital"] = np.where(
-        conteo_procedimientos["diferencia_proximo_hospital"] < 0,
-        -1,
-        conteo_procedimientos["diferencia_proximo_hospital"],
-    )
+    total_procedimientos = conteo_procedimientos.groupby(["ANIO_EGRESO", "procedimiento"])[
+        "conteo"
+    ].transform("sum")
+
+    conteo_procedimientos["ranking"] = ranking_por_procedimiento
+    conteo_procedimientos["diferencia_proximo_hospital"] = diferencia_entre_hospitales_contiguos
+    conteo_procedimientos["total_procedimientos"] = total_procedimientos
 
     return conteo_procedimientos
