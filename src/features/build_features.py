8bc7eecb55cf9d0f9be8362082dbb1e41d93806d
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-09-07 10:43:45 -0300

    Agrega funciones sociodemograficas

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -41,3 +41,82 @@ def calcular_conteo_y_ranking_sobre_variable_categorica(
     conteo[var_proporcion] = conteo[var_conteo] / conteo[var_totales]
 
     return conteo
+
+
+def conteo_agrupado_de_variable(
+    df, vars_groupby, col_a_contar, cols_para_llave, variable_analizada
+):
+    """
+    Perform grouped counting and aggregation of a variable in a DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame.
+        vars_groupby (list[str]): Columns by which to group the DataFrame.
+        col_a_contar (str): Column containing values to be counted.
+        cols_para_llave (list[str]): Columns to be used as keys for aggregation.
+        variable_analizada (str): Name of the variable being analyzed.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing grouped counts and aggregated data.
+    """
+    columnas_finales = vars_groupby + [col_a_contar]
+    if not all(col in columnas_finales for col in cols_para_llave):
+        raise ValueError(
+            "Tus columnas para hacer la llave estan ausentes en las columnas finales. "
+            "Debes utilizar columnas que esten en la agrupacion + variable de conteo "
+            "para hacer una llave valida"
+        )
+
+    if df.empty:
+        return None
+
+    resultado = (
+        df.groupby(vars_groupby, dropna=False)[col_a_contar]
+        .value_counts()
+        .reset_index(name=f"conteo_{variable_analizada}")
+    )
+
+    resultado["llave_id"] = resultado[cols_para_llave].astype(str).apply("-".join, axis=1)
+    return resultado
+
+
+def obtener_desglose_sociodemografico(
+    df, vars_groupby_estatico, vars_groupby_dinamico, col_a_contar
+):
+    """
+    Obtain demographic breakdown of a specified variable within static and dynamic groups.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame.
+        vars_groupby_estatico (list[str]): Columns for static grouping.
+        vars_groupby_dinamico (list[str]): Columns for dynamic grouping.
+        col_a_contar (str): Column containing values to be counted.
+
+    Returns:
+        dict: A dictionary containing breakdown results for static and dynamic groups.
+    """
+
+    # Aqui se va a tener un groupby estatico. Ej: Anios
+    # Se va a tener un groupby dinamico. Ej: Sexo, Otros
+    # Y se va a tener una variable que se va a contar. Ej: Diagnostico
+
+    dict_resultados = {}
+    cols_para_llave = vars_groupby_estatico + [col_a_contar]
+
+    conteo_global = conteo_agrupado_de_variable(
+        df, vars_groupby_estatico, col_a_contar, cols_para_llave, "global"
+    )
+
+    dict_resultados["global"] = conteo_global
+
+    for variable in vars_groupby_dinamico:
+        # Por lo tanto, aqui el groupby que se hara sera ["Anios", "Sexo"].
+        desglose_dinamico = vars_groupby_estatico + [variable]
+        conteo_dinamico = conteo_agrupado_de_variable(
+            df, desglose_dinamico, col_a_contar, cols_para_llave, variable
+        )
+        conteo_dinamico = conteo_dinamico.drop(columns=cols_para_llave)
+
+        dict_resultados[variable] = conteo_dinamico
+
+    return dict_resultados
